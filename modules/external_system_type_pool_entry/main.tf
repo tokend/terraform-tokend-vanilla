38e60400c0419f1c2290f5c71bcd07a3fe98279b
Levchks <tg.levochko@gmail.com>
2019-07-24 12:45:46 +0300

    add external system type module

---
+++ modules/external_system_type_pool_entry/main.tf
@@ -0,0 +1,30 @@
+
+resource tokend_key_value "external_system_type_stellar" {
+  key        = "external_system_type_stellar"
+  value_type = "uint32"
+  value      = "1"
+}
+
+resource tokend_key_value "external_system_type_ethereum" {
+  key        = "external_system_type_ethereum"
+  value_type = "uint32"
+  value      = "2"
+}
+
+resource "tokend_external_system_pool_entry" "test_eth" {
+  depends_on = ["tokend_key_value.external_system_type_ethereum"]
+  target_count = 1
+  external_system_type = "${tokend_key_value.external_system_type_ethereum.value}"
+  data = {
+    x_pub = "tpubD6NzVbkrYhZ4Xcr6EpCgsKYmwy8yScpU2uDPJJ61DXjWQ7YBj9WWBGetVJu3cwXWsyAxmc5AP3EMfuYALUWf6Hjwrz4eRrTxqa8g8YN8kiT"
+  }
+}
+
+resource "tokend_external_system_pool_entry" "test_stellar" {
+  depends_on = ["tokend_key_value.external_system_type_stellar"]
+  target_count = 1
+  external_system_type = "${tokend_key_value.external_system_type_stellar.value}"
+  data = {
+    address = "tpubD6NzVbkrYhZ4Xcr6EpCgsKYmwy8yScpU2uDPJJ61DXjWQ7YBj9WWBGetVJu3cwXWsyAxmc5AP3EMfuYALUWf6Hjwrz4eRrTxqa8g8YN8kiT"
+  }
+}
