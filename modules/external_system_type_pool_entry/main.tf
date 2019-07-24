424cad4e6f5f4519b75b9569cf9e72d37c06b6e7
Levchks <tg.levochko@gmail.com>
2019-07-24 15:05:09 +0300

    fix key name

--- modules/external_system_type_pool_entry/main.tf
+++ modules/external_system_type_pool_entry/main.tf
@@ -1,12 +1,11 @@
-
 resource tokend_key_value "external_system_type_stellar" {
-  key        = "external_system_type_stellar"
+  key        = "external_system_type:stellar"
   value_type = "uint32"
   value      = "1"
 }
 
 resource tokend_key_value "external_system_type_ethereum" {
-  key        = "external_system_type_ethereum"
+  key        = "external_system_type:ethereum"
   value_type = "uint32"
   value      = "2"
 }
