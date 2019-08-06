91d252d10133e78d2a154ed06ab38d8f348c1540
Anton <antonymartynov@gmail.com>
2019-08-06 20:08:42 +0300

    Updated amount of generated stellar addresses

--- modules/external_system_type_pool_entry/main.tf
+++ modules/external_system_type_pool_entry/main.tf
@@ -12,7 +12,7 @@ resource tokend_key_value "external_system_type_ethereum" {
 
 resource "tokend_external_system_pool_entry" "test_stellar" {
   depends_on = ["tokend_key_value.external_system_type_stellar"]
-  target_count = 1
+  target_count = 1000
   external_system_type = "${tokend_key_value.external_system_type_stellar.value}"
   data = {
     address = "tpubD6NzVbkrYhZ4Xcr6EpCgsKYmwy8yScpU2uDPJJ61DXjWQ7YBj9WWBGetVJu3cwXWsyAxmc5AP3EMfuYALUWf6Hjwrz4eRrTxqa8g8YN8kiT"
