c0f2d00c886fbbf52370de8cee9536bbe120a988
Anton <antonymartynov@gmail.com>
2019-08-21 17:30:36 +0300

    Made UAH the only currency

--- modules/assets/main.tf
+++ modules/assets/main.tf
@@ -1,12 +1,11 @@
-resource tokend_asset "DOGE" {
-  code                        = "DOGE"
+resource tokend_asset "UAH" {
+  code                        = "UAH"
   max_issuance_amount         = "9223372036853"
   initial_pre_issuance_amount = "9223372036853"
-  pre_issuance_signer         = "GBA4EX43M25UPV4WIE6RRMQOFTWXZZRIPFAI5VPY6Z2ZVVXVWZ6NEOOB"
   trailing_digits_count       = 6
 
   details = {
-    name = "Dogecoin"
+    name = "Ukrainian hryvnia"
   }
 
   policies = [
@@ -14,55 +13,4 @@ resource tokend_asset "DOGE" {
     "base_asset",
     "stats_quote_asset",
   ]
-}
-
-resource tokend_asset "BTC" {
-  code                        = "BTC"
-  max_issuance_amount         = "21000000"
-  initial_pre_issuance_amount = "21000000"
-  pre_issuance_signer         = "GBA4EX43M25UPV4WIE6RRMQOFTWXZZRIPFAI5VPY6Z2ZVVXVWZ6NEOOB"
-  trailing_digits_count       = 6
-
-  details = {
-    name = "Bitcoin"
-  }
-
-  policies = [
-    "transferable",
-    "base_asset",
-  ]
-}
-
-resource tokend_asset "USD" {
-  code                        = "USD"
-  max_issuance_amount         = "9223372036853"
-  initial_pre_issuance_amount = "9223372036853"
-  pre_issuance_signer         = "GBA4EX43M25UPV4WIE6RRMQOFTWXZZRIPFAI5VPY6Z2ZVVXVWZ6NEOOB"
-  trailing_digits_count       = 6
-
-  details = {
-    name = "US Dollar"
-  }
-
-  policies = [
-    "transferable",
-  ]
-}
-
-resource tokend_asset_pair "BTC_DOGE" {
-  base  = "${tokend_asset.BTC.id}"
-  quote = "${tokend_asset.DOGE.id}"
-  price = "1960909"
-}
-
-resource tokend_asset_pair "USD_DOGE" {
-  base  = "${tokend_asset.USD.id}"
-  quote = "${tokend_asset.DOGE.id}"
-  price = "1000"
-}
-
-resource tokend_asset_pair "USD_BTC" {
-  base  = "${tokend_asset.BTC.id}"
-  quote = "${tokend_asset.USD.id}"
-  price = "2344111116"
 }
