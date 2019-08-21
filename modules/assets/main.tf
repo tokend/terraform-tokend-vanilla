0a7c102aae9fba71b1e86bff50aca463a853353d
Anton <antonymartynov@gmail.com>
2019-08-21 18:00:33 +0300

    Returned pre_issuance_signer

--- modules/assets/main.tf
+++ modules/assets/main.tf
@@ -2,6 +2,7 @@ resource tokend_asset "UAH" {
   code                        = "UAH"
   max_issuance_amount         = "9223372036853"
   initial_pre_issuance_amount = "9223372036853"
+  pre_issuance_signer         = "GAB4FEAPIFH7KRUZQYR6I3VKKRQHGZGAOOPO56JIUPNKY2W5T6SMPWGD"
   trailing_digits_count       = 6
 
   details = {
