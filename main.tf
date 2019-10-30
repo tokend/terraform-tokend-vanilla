cb2bd9092b4efd96fb4a36fe8703ee954823f22a
Yaroslav Panasenko <yar.panasenko@gmail.com>
2019-10-30 12:46:23 +0200

    Removed external system account pool entries scripts

--- main.tf
+++ main.tf
@@ -274,10 +274,6 @@ module "assets" {
   source = "modules/assets"
 }
 
-module "external_system_type_pool_entry" {
-  source = "modules/external_system_type_pool_entry"
-}
-
 module "signers" {
   source = "modules/signers"
   license_signer_role = "${module.signer_roles.license_signer_role}"
