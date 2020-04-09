8f2fe2c273a538822a41f1788fbdb36acc81ba69
Yaroslav Panasenko <yar.panasenko@gmail.com>
2020-04-09 14:06:25 +0300

    Removed unused values

--- modules/account_roles/main.tf
+++ modules/account_roles/main.tf
@@ -2,23 +2,7 @@ variable unverified_rules {
   type = "list"
 }
 
-variable general_rules {
-  type = "list"
-}
-
-variable syndicate_rules {
-  type = "list"
-}
-
 variable blocked_rules {
-  type = "list"
-}
-
-variable us_verified {
-  type = "list"
-}
-
-variable us_accredited {
   type = "list"
 }
 
@@ -26,24 +10,8 @@ resource tokend_account_role "unverified" {
   rules = ["${var.unverified_rules}"]
 }
 
-resource tokend_account_role "general" {
-  rules = ["${var.general_rules}"]
-}
-
-resource tokend_account_role "syndicate" {
-  rules = ["${var.syndicate_rules}"]
-}
-
 resource tokend_account_role "blocked" {
   rules = ["${var.blocked_rules}"]
-}
-
-resource tokend_account_role "us_accredited" {
-  rules = ["${var.us_accredited}"]
-}
-
-resource tokend_account_role "us_verified" {
-  rules = ["${var.us_verified}"]
 }
 
 resource tokend_key_value "unverified" {
@@ -52,32 +20,8 @@ resource tokend_key_value "unverified" {
   value = "${tokend_account_role.unverified.id}"
 }
 
-resource tokend_key_value "general" {
-  key = "account_role:general"
-  value_type = "uint32"
-  value = "${tokend_account_role.general.id}"
-}
-
-resource tokend_key_value "syndicate" {
-  key = "account_role:corporate"
-  value_type = "uint32"
-  value = "${tokend_account_role.syndicate.id}"
-}
-
 resource tokend_key_value "blocked" {
   key = "account_role:blocked"
   value_type = "uint32"
   value = "${tokend_account_role.blocked.id}"
-}
-
-resource tokend_key_value "us_verified" {
-  key = "account_role:us_verified"
-  value_type = "uint32"
-  value = "${tokend_account_role.us_verified.id}"
-}
-
-resource tokend_key_value "us_accredited" {
-  key = "account_role:us_accredited"
-  value_type = "uint32"
-  value = "${tokend_account_role.us_accredited.id}"
 }
--- modules/signer_roles/main.tf
+++ modules/signer_roles/main.tf
@@ -1,45 +1,3 @@
-variable "default_rules" {
-  type = "list"
-}
-
-variable "kyc_aml_admin" {
-  type = "list"
-}
-
-variable "license_admin" {
-  type = "list"
-}
-
-variable "create_kyc" {
-  type = "list"
-}
-
-variable "issuance_signer" {
-  type = "list"
-}
-
-resource tokend_signer_role "issuance_signer" {
-  rules = [
-    "${var.issuance_signer}",
-  ]
-  details = {
-    admin_role = false
-    name = "Issuance"
-    description = "Role allows signer only to send create issuance requests"
-  }
-}
-
-resource tokend_signer_role "create_kyc_recovery" {
-  rules = [
-  "${var.create_kyc}",
-  ]
-  details = {
-    admin_role = false
-    name = "KYC Recovery creator"
-    description = "Use ID of role as value in kv by kyc_recovery_signer_role key"
-  }
-}
-
 resource tokend_signer_role "super_admin" {
   rules = [
     "1",
@@ -49,29 +7,6 @@ resource tokend_signer_role "super_admin" {
     admin_role  = true
     name        = "Super Administrator"
     description = "Have full access to system administration functionality"
-  }
-}
-
-resource tokend_signer_role "kyc_aml_admin" {
-  rules = [
-    "${var.kyc_aml_admin}",
-  ]
-
-  details = {
-    admin_role  = true
-    name        = "KYC/AML"
-    description = "Responsible for reviewing users requests including KYC, asset/sale creation etc"
-  }
-}
-
-resource tokend_signer_role "license_admin" {
-  rules = [
-  "${var.license_admin}"
-  ]
-  details = {
-    admin_role = true
-    name = "License Admin"
-    description = "Able to manage system licenses"
   }
 }
 
@@ -91,21 +26,11 @@ resource tokend_key_value "default" {
 resource tokend_key_value "create_kyc_recovery_role" {
   key        = "kyc_recovery_signer_role"
   value_type = "uint64"
-  value      = "${tokend_signer_role.create_kyc_recovery.id}"
-}
-
-resource tokend_key_value "issuance_signer_role" {
-  key        = "signer_role:issuance"
-  value_type = "uint32"
-  value      = "${tokend_signer_role.issuance_signer.id}"
+  value      = "9999"
 }
 
 resource tokend_key_value "license_admin_role" {
   key        = "license_admin_signer_role"
   value_type = "uint64"
-  value      = "${tokend_signer_role.license_admin.id}"
-}
-
-output "license_signer_role" {
-  value = "${tokend_signer_role.license_admin.id}"
+  value      = "9999"
 }
