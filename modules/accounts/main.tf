c5f23a9e31286352b9af51e1a10b56ae50ced364
hennadiip <hennadii.ponedielnikov@gmail.com>
2020-04-11 19:21:32 +0300

    tryna add signer correctly 2

--- modules/accounts/main.tf
+++ modules/accounts/main.tf
@@ -9,7 +9,5 @@ variable "rec_payments_account_signers" {
 resource tokend_account "recurring_payments_account" {
     account_id = "GBSO4E7YBNWOGQAW64RC4LACM3YNBH2C4PWRLNXILVFHG2AWANH54L5T" # SC45C2RCEBVIUMKU6MJBVDI2PULM4RXJBNGRZRSGRZRVIK5UZMFUFOIX
     role_id = "${var.general_account_role}"
-    signers = [
-        "${var.rec_payments_account_signers}"
-    ]
+    signers = "${var.rec_payments_account_signers}"
 }
