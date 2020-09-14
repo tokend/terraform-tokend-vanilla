7c12dcd544342deb12e8f2bddaa46c1aadba27ab
Sergey Gimonov <xr9kayu@gmail.com>
2020-09-14 15:50:31 +0300

    add data_type and data_task

--- modules/key_values/main.tf
+++ modules/key_values/main.tf
@@ -25,9 +25,9 @@ resource tokend_key_value "asset_type_kyc_required" {
 }
 
 resource tokend_key_value "asset_type_default" {
-    key = "asset_type:default"
-    value_type = "uint32"
-    value = "${var.asset_type_default}"
+  key = "asset_type:default"
+  value_type = "uint32"
+  value = "${var.asset_type_default}"
 }
 
 resource tokend_key_value "asset_type_nft" {
@@ -37,87 +37,87 @@ resource tokend_key_value "asset_type_nft" {
 }
 
 resource tokend_key_value "change_role_task-submit_auto_verification" {
-    key = "change_role_task:submit_auto_verification"
-    value_type = "uint32"
-    value = "1024"
+  key = "change_role_task:submit_auto_verification"
+  value_type = "uint32"
+  value = "1024"
 }
 
 resource tokend_key_value "change_role_task-complete_auto_verification" {
-    key = "change_role_task:complete_auto_verification"
-    value_type = "uint32"
-    value = "2048"
+  key = "change_role_task:complete_auto_verification"
+  value_type = "uint32"
+  value = "2048"
 }
 
 resource tokend_key_value "change_role_task-manual_review_required" {
-    key = "change_role_task:manual_review_required"
-    value_type = "uint32"
-    value = "4096"
+  key = "change_role_task:manual_review_required"
+  value_type = "uint32"
+  value = "4096"
 }
 
 resource tokend_key_value "default_withdraw_tasks" {
-  key        = "withdrawal_tasks:*"
+  key = "withdrawal_tasks:*"
   value_type = "uint32"
-  value      = "2048"
+  value = "2048"
 }
 
 resource tokend_key_value "default_change_role_tasks" {
-  key        = "change_role_tasks:*:*"
+  key = "change_role_tasks:*:*"
   value_type = "uint32"
-  value      = "1"
+  value = "1"
 }
 
 resource tokend_key_value "sign_up_change_role_task" {
-  key        = "change_role_tasks:${var.account_unverified}:${var.account_general}"
+  key = "change_role_tasks:${var.account_unverified}:${var.account_general}"
   value_type = "uint32"
-  value      = "0"
+  value = "0"
 }
 
 resource tokend_key_value "asset_create_tasks" {
-  key        = "asset_create_tasks"
+  key = "asset_create_tasks"
   value_type = "uint32"
-  value      = "1"
+  value = "1"
 }
 
 resource tokend_key_value "limits_update_tasks" {
-  key        = "limits_update_tasks"
+  key = "limits_update_tasks"
   value_type = "uint32"
-  value      = "1"
+  value = "1"
 }
 
 resource tokend_key_value "asset_update_default" {
-  key        = "asset_update_tasks:*"
+  key = "asset_update_tasks:*"
   value_type = "uint32"
-  value      = "1"
+  value = "1"
 }
 
 resource tokend_key_value "asset_update_task" {
-  key        = "asset_update_tasks"
+  key = "asset_update_tasks"
   value_type = "uint32"
-  value      = "1"
+  value = "1"
 }
 
 resource tokend_key_value "sale_create_tasks" {
-  key        = "sale_create_tasks:*"
+  key = "sale_create_tasks:*"
   value_type = "uint32"
-  value      = "2"
+  value = "2"
 }
 
 resource tokend_key_value "issuance_tasks_default" {
-  key        = "issuance_tasks:*"
+  key = "issuance_tasks:*"
   value_type = "uint32"
-  value      = "0"
+  value = "0"
 }
 
 resource tokend_key_value "preissuance_tasks_default" {
-  key        = "preissuance_tasks:*"
+  key = "preissuance_tasks:*"
   value_type = "uint32"
-  value      = "0"
+  value = "0"
 }
 
 resource tokend_key_value "kyc_recovery_enabled" {
-  key        = "kyc_recovery_enabled"
+  key = "kyc_recovery_enabled"
   value_type = "uint32"
-  value      = "1"
+  value = "1"
 }
 
 resource tokend_key_value "create_kyc_recovery_tasks" {
@@ -136,4 +136,28 @@ resource tokend_key_value "ext_sys_exp_period_erc20" {
   key = "ext_sys_exp_period:3"
   value_type = "uint32"
   value = "1200"
+}
+
+resource tokend_key_value "ext_sys_exp_period_erc20" {
+  key = "ext_sys_exp_period:3"
+  value_type = "uint32"
+  value = "1200"
+}
+
+resource tokend_key_value "data_type_identity" {
+  key = "data_type:identity"
+  value_type = "uint32"
+  value = 1
+}
+
+resource tokend_key_value "data_type_identity_type" {
+  key = "data_type:identity_type"
+  value_type = "uint32"
+  value = 2
+}
+
+resource tokend_key_value "data_task_identity" {
+  key = "data_task:identity"
+  value_type = "uint32"
+  value = 3
 }
