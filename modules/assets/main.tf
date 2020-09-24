7c0dedae530132d71b54f33cb76ba693a424e825
alxwnth <69579692+alxwnth@users.noreply.github.com>
2020-09-24 16:59:01 +0300

    Update main.tf

--- modules/assets/main.tf
+++ modules/assets/main.tf
@@ -2,7 +2,7 @@ resource tokend_asset "BTC" {
   code                        = "BTC"
   max_issuance_amount         = "21000000"
   initial_pre_issuance_amount = "21000000"
-  pre_issuance_signer         = "GBA4EX43M25UPV4WIE6RRMQOFTWXZZRIPFAI5VPY6Z2ZVVXVWZ6NEOOB"
+  pre_issuance_signer         = "GCBIE4BC66Y3X3H5Z5TSWKYKCMCSYVRUY54EBGRX7PMVSBOXMYNBDGTF"
   trailing_digits_count       = 6
 
   details = {
@@ -12,23 +12,41 @@ resource tokend_asset "BTC" {
   policies = [
     "transferable",
     "base_asset",
+    "withdrawable"
   ]
 }
-
-resource tokend_asset "EUR" {
-  code                        = "EUR"
+resource tokend_asset "GSHI" {
+  code                        = "GSHI"
   max_issuance_amount         = "9223372036853"
   initial_pre_issuance_amount = "9223372036853"
-  pre_issuance_signer         = "GBA4EX43M25UPV4WIE6RRMQOFTWXZZRIPFAI5VPY6Z2ZVVXVWZ6NEOOB"
+  pre_issuance_signer         = "GCBIE4BC66Y3X3H5Z5TSWKYKCMCSYVRUY54EBGRX7PMVSBOXMYNBDGTF"
   trailing_digits_count       = 2
 
   details = {
-    name = "Euro"
+    name = "GSH€"
   }
 
   policies = [
     "transferable",
     "base_asset",
     "stats_quote_asset"
+  ]
+}
+resource tokend_asset "GSHD" {
+  code                        = "GSHD"
+  max_issuance_amount         = "9223372036853"
+  initial_pre_issuance_amount = "9223372036853"
+  pre_issuance_signer         = "GCBIE4BC66Y3X3H5Z5TSWKYKCMCSYVRUY54EBGRX7PMVSBOXMYNBDGTF"
+  trailing_digits_count       = 2
+  type                        = 3
+
+  details = {
+    name = "GSH€"
+  }
+
+  policies = [
+    "transferable",
+    "base_asset",
+    "withdrawable"
   ]
 }
