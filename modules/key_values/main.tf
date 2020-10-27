a7917df55a549efabacd6f1dbbb8492d74e52871
Vladislav Logvinenko <logvinenko.js@gmail.com>
2020-10-27 19:59:59 +0200

    Remove tasks from asset update

--- modules/key_values/main.tf
+++ modules/key_values/main.tf
@@ -133,13 +133,13 @@ resource tokend_key_value "limits_update_tasks" {
 resource tokend_key_value "asset_update_default" {
   key        = "asset_update_tasks:*"
   value_type = "uint32"
-  value      = "1"
+  value      = "0"
 }
 
 resource tokend_key_value "asset_update_task" {
   key        = "asset_update_tasks"
   value_type = "uint32"
-  value      = "1"
+  value      = "0"
 }
 
 resource tokend_key_value "sale_create_tasks" {
