357c14d6177a0e4d92360357cce0f3416a45732e
hp <hennadii.ponedielnikov@gmail.com>
2021-02-02 01:08:39 +0200

    empty signers

--- modules/accounts/main.tf
+++ modules/accounts/main.tf
@@ -9,4 +9,5 @@ variable "general_account_role" {
 resource tokend_account "general_account" {
   account_id = "GC5YTANSNAF2V4QW3QRRXQV4KWDALLPY5XM4GJUIT3NB2RYV3BTREWPI" # SA6CAKDXNLYDUFYNXDS7FRVPR3QKMOK3SGKZMPLBQDAHPO2A5KZH56OK
   role_id = "${var.general_account_role}"
+  signers = []
 }
