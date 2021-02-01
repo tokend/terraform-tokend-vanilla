cc105f175197c67589efa7a55c557b4345a7d2d8
hp <hennadii.ponedielnikov@gmail.com>
2021-02-02 00:01:52 +0200

    added temporary account for debugging purposes

--- modules/accounts/main.tf
+++ modules/accounts/main.tf
@@ -7,7 +7,7 @@ variable "general_account_role" {
 }
 
 resource tokend_account "general_account" {
-  public_key = "GAXZ6MNYLF7AV3YK2TGTETJ4PCZQT3CQTUTYRFSYDWRQZJNI34H3L74V"
+  public_key = "GC5YTANSNAF2V4QW3QRRXQV4KWDALLPY5XM4GJUIT3NB2RYV3BTREWPI" # SA6CAKDXNLYDUFYNXDS7FRVPR3QKMOK3SGKZMPLBQDAHPO2A5KZH56OK
   role_id = "${var.general_account_role}"
   signers = [
     "${var.general_account_signers}"
