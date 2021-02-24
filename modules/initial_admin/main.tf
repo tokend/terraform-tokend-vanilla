1082f101653e2d288896283716f21385394d48b2
Yaroslav Stepanchuk <yaroslav.stepanchuk@trdata.com>
2021-02-24 21:48:57 +0200

    Use dependency on key-values

--- modules/initial_admin/main.tf
+++ modules/initial_admin/main.tf
@@ -6,4 +6,9 @@ resource tokend_initial_admin "admin" {
   job_function = "1"
   job_title    = "Master Admin"
   role_id      = "7"
+
+  depends_on = [
+    "tokend_account_role.master_admin",
+    "tokend_key_value.kyc_recovery_enabled"
+  ]
 }
