b5581a536b90bcc6d8d69df3a7acfe15d31167ad
Yaroslav Stepanchuk <yaroslav.stepanchuk@trdata.com>
2021-02-24 21:41:35 +0200

    Revert "Remove initial admin resource"
    This reverts commit 88e02151cf5cbf5ef9ad0142bbaed693c06ac3d1.

---
+++ modules/initial_admin/main.tf
@@ -0,0 +1,9 @@
+resource tokend_initial_admin "admin" {
+  email        = "oleg.k@distributedlab.com"
+  title        = "Mr"
+  first_name   = "Master"
+  last_name    = "Admin"
+  job_function = "1"
+  job_title    = "Master Admin"
+  role_id      = "7"
+}
