2ed1cb3cfff4470d196777e948d8ed3fe13e609d
Yaroslav Stepanchuk <yaroslav.stepanchuk@trdata.com>
2021-03-01 14:15:44 +0200

    Remove resource initial_admin

--- modules/initial_admin/main.tf
+++
@@ -1,9 +0,0 @@
-resource tokend_initial_admin "admin" {
-  email        = "torwigua@gmail.com"
-  title        = "Mr"
-  first_name   = "Master"
-  last_name    = "Admin"
-  job_function = "1"
-  job_title    = "Master Admin"
-  role_id      = "6"
-}
