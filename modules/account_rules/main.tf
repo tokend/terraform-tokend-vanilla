63b4f316527802c3bc107cbb3376f1139e3d0c86
Sergey Gimonov <xr9kayu@gmail.com>
2021-07-08 18:49:19 +0300

    update it

--- modules/account_rules/main.tf
+++ modules/account_rules/main.tf
@@ -79,13 +79,13 @@ resource tokend_account_rule "reviewable_request_creator" { // TODO shrink scope
 }
 
 
-resource tokend_account_rule "reviewable_request_creator_with_tasks" { // TODO shrink scope to specific requests
-  action = "create_with_tasks"
-  entry_type = "reviewable_request"
-  entry = {
-    request_type = "create_issuance"
-  }
-}
+//resource tokend_account_rule "reviewable_request_creator_with_tasks" { // TODO shrink scope to specific requests
+//  action = "create_with_tasks"
+//  entry_type = "reviewable_request"
+//  entry = {
+//    request_type = "create_issuance"
+//  }
+//}
 
 
 
@@ -196,9 +196,9 @@ output "reviewable_request_creator" {
   value = "${tokend_account_rule.reviewable_request_creator.id}"
 }
 
-output "reviewable_request_creator_with_tasks" {
-  value = "${tokend_account_rule.reviewable_request_creator_with_tasks.id}"
-}
+//output "reviewable_request_creator_with_tasks" {
+//  value = "${tokend_account_rule.reviewable_request_creator_with_tasks.id}"
+//}
 
 output "signer_manager" {
   value = "${tokend_account_rule.signer_manager.id}"
