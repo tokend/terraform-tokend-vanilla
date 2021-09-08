60ffe2565fde9f27cad01aebbe6b45f5d63d7725
Sergey Gimonov <xr9kayu@gmail.com>
2021-09-08 16:31:09 +0300

    rename USDJ to JUSD; add USDT

--- modules/assets/main.tf
+++ modules/assets/main.tf
@@ -1,15 +1,32 @@
 resource tokend_asset "USDJ" {
-  code                        = "USDJ"
+  code                        = "JUSD"
   max_issuance_amount         = "9223372036853"
   initial_pre_issuance_amount = "9223372036853"
   pre_issuance_signer         = "GBA4EX43M25UPV4WIE6RRMQOFTWXZZRIPFAI5VPY6Z2ZVVXVWZ6NEOOB"
   trailing_digits_count       = 6
 
   details = {
-    name = "USDJ"
+    name = "JUSD"
   }
 
   policies = [
     "transferable",
+  ]
+}
+
+resource tokend_asset "USDT" {
+  code                        = "JUSD"
+  max_issuance_amount         = "9223372036853"
+  initial_pre_issuance_amount = "9223372036853"
+  pre_issuance_signer         = "GBA4EX43M25UPV4WIE6RRMQOFTWXZZRIPFAI5VPY6Z2ZVVXVWZ6NEOOB"
+  trailing_digits_count       = 6
+
+  details = {
+    name = "JUSD"
+  }
+
+  policies = [
+    "transferable",
+    "stats_quote_asset",
   ]
 }
