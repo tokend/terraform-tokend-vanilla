795dde8f465f4b0e0ba07f10680bb8356ee9c665
ivanlele <ivanlele402@gmail.com>
2022-08-24 15:47:33 +0000

    fix

--- main.tf
+++ main.tf
@@ -225,6 +225,48 @@ module "account_roles" {
   ]
 
   blocked_rules = []
+
+  nft_admin_rules = [
+    "${module.account_rules.balance_creator}",
+    "${module.account_rules.sender}",
+    "${module.account_rules.payment_receiver}",
+    "${module.account_rules.atomic_swap_receiver}",
+    "${module.account_rules.issuance_receiver}",
+    "${module.account_rules.tx_sender}",
+    "${module.account_rules.role_updater}",
+    "${module.account_rules.signer_manager}",
+    "${module.account_rules.default_for_default_sell_offer_creator}",
+    "${module.account_rules.default_for_default_buy_offer_creator}",
+    "${module.account_rules.default_for_kyc_buy_offer_creator}",
+    "${module.account_rules.default_for_kyc_sell_offer_creator}",
+    "${module.account_rules.kyc_for_kyc_buy_offer_creator}",
+    "${module.account_rules.kyc_for_kyc_sell_offer_creator}",
+    "${module.account_rules.kyc_for_default_buy_offer_creator}",
+    "${module.account_rules.kyc_for_default_sell_offer_creator}",
+    "${module.account_rules.security_for_default_buy_offer_creator}",
+    "${module.account_rules.security_for_default_sell_offer_creator}",
+    "${module.account_rules.security_for_kyc_buy_offer_creator}",
+    "${module.account_rules.security_for_kyc_sell_offer_creator}",
+    "${module.account_rules.security_for_security_buy_offer_creator}",
+    "${module.account_rules.security_for_security_sell_offer_creator}",
+    "${module.account_rules.default_for_security_buy_offer_creator}",
+    "${module.account_rules.default_for_security_sell_offer_creator}",
+    "${module.account_rules.kyc_for_security_buy_offer_creator}",
+    "${module.account_rules.kyc_for_security_sell_offer_creator}",
+    "${module.account_rules.kyc_sender}",
+    "${module.account_rules.kyc_atomic_swap_receiver}",
+    "${module.account_rules.kyc_payment_receiver}",
+    "${module.account_rules.kyc_withdrawer}",
+    "${module.account_rules.kyc_issuance_receiver}",
+    "${module.account_rules.reviewable_request_creator}",
+    "${module.account_rules.sale_participant}",
+    "${module.account_rules.asset_withdrawer}",
+    "${module.account_rules.external_binder}",
+    "${module.account_rules.vote_creator}",
+    "${module.account_rules.vote_remover}",
+    "${module.account_rules.forbid_restricted_vote_remove}",
+    "${module.account_rules.kyc_recovery_creator}",
+  ]
 }
 
 // create defaul signer rules
